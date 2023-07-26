2e0b8e7e01f3a465c19cd74e1a5422a6c49b5500
n.zitouni <n.zitouni@capency.com>
2023-07-26 17:21:41 +0200

    implement product sheet + refactore css + add bs

--- src/app/product-sheet/product-sheet.component.ts
+++ src/app/product-sheet/product-sheet.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-sheet',
@@ -6,7 +12,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-sheet.component.scss'],
 })
 export class ProductSheetComponent implements OnInit {
-  constructor() {}
+  faLeft = faArrowAltCircleLeft;
+  faRight = faArrowAltCircleRight;
+  slides: any = [
+    { id: 'K' },
+    { id: 'L' },
+    { id: 'M' },
+    { id: 'N' },
+    { id: 'O' },
+  ];
+  quantity: number = 1;
+  productForm: FormGroup;
 
-  ngOnInit(): void {}
+  getPrevIndex(index: number): number {
+    return (index - 1 + this.slides.length) % this.slides.length;
+  }
+
+  getNextIndex(index: number): number {
+    return (index + 1) % this.slides.length;
+  }
+  constructor(private fb: FormBuilder, private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      quantity: [this.quantity],
+    });
+  }
+
+  addToCart() {
+    this.cartService.addItemToCart();
+  }
+
+  increment() {
+    this.productForm.patchValue({
+      quantity: this.productForm.value.quantity + 1,
+    });
+  }
+
+  decrement() {
+    if (this.productForm.value.quantity > 1) {
+      this.productForm.patchValue({
+        quantity: this.productForm.value.quantity - 1,
+      });
+    }
+  }
 }
